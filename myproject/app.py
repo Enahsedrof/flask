6e0b2f13836ab75a2869dd938832fc353ac376e8
Shane Forde <shaneforde02@googlemail.com>
2021-05-11 12:35:43 +0100

    SF: fixed myproject DB connection

--- myproject/app.py
+++ myproject/app.py
@@ -1,16 +1,17 @@
 from flask import Flask # Import Flask class
 from flask_sqlalchemy import SQLAlchemy # Import SQLAlchemy class
 
+
 app = Flask(__name__) #create Flask object
 
-app.config['SQLALCHEMY_DATABASE_URI'] = '' #Set the connection string to connect to the database
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@35.246.66.73:3306/flask_external' #Set the connection string to connect to the database
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app) # create SQLAlchemy opject
 
-class Users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(30), nullable=False)
-    last_name = db.Column(db.String(30), nullable=False)
+# class Users(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     first_name = db.Column(db.String(30), nullable=False)
+#     last_name = db.Column(db.String(30), nullable=False)
 
 if __name__=='__main__':
     app.run(debug==True, host='0.0.0.0')
