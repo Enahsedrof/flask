efe46061c2e2973e3dc847b84a6e713eac310bc3
Shane Forde <shaneforde02@googlemail.com>
2021-05-11 12:44:03 +0100

    SF: added information

--- myproject/app.py
+++ myproject/app.py
@@ -8,10 +8,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@35.246.66.73:
 #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app) # create SQLAlchemy opject
 
-# class Users(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     first_name = db.Column(db.String(30), nullable=False)
-#     last_name = db.Column(db.String(30), nullable=False)
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
 
 if __name__=='__main__':
     app.run(debug==True, host='0.0.0.0')
