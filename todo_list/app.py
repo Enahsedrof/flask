63ba88c27ebe571cbe611e0d453c6f50e2462d15
Shane Forde <shaneforde02@googlemail.com>
2021-05-12 16:47:51 +0100

    SF: tried to get fancy, working the kinks

--- todo_list/app.py
+++ todo_list/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@35.246.66.73:3306/todo'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@35.197.238.11/todo'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -14,7 +14,30 @@ class Todo(db.Model):
 
 @app.route("/")
 def index():
-    return "This is a TODO app"
+    todo_list = Todo.query.all()
+    return render_template("index.html", todo_list=todo_list)
+
+@app.route("/add", methods=["POST"])
+def add():
+    task = request.form.get("task")
+    new_todo = Todo(task=task, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
 
 if __name__ == '__main__':
     app.run(debug=True, host = '0.0.0.0')
