fa96f9942ac40d030448d68e18d80412ee34e88a
Shane Forde <shaneforde02@googlemail.com>
2021-05-12 14:27:25 +0100

    SF: added forms

--- template/app.py
+++ template/app.py
@@ -10,5 +10,10 @@ def ben():
 def harry():
     return render_template('harry.html')
 
+@app.route('/list')
+def _list():
+    list_1 = ["Alpha", "Beta", "Pi", "Epsilon"]
+    return render_template("list.html", users=list_1)
+
 if __name__ == "__main__":
     app.run(debug=True)
