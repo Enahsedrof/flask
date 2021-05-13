c85a68657ea4da0bcd405220ce1003f3a4c2b294
Shane Forde <shaneforde02@googlemail.com>
2021-05-13 16:10:17 +0100

    SF: fixed add page, add and delete functions now work, add checkbox

--- todo_list/app.py
+++ todo_list/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@35.197.238.11/todo'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SECRET_KEY"] = "ererhe"
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
@@ -12,18 +15,24 @@ class Todo(db.Model):
     task = db.Column(db.String(50), nullable=False)
     complete = db.Column(db.Boolean, default=False)
 
+class TodoForm(FlaskForm):
+    task = StringField("Task")
+    submit = SubmitField("Add Todo")
+
 @app.route("/")
 def index():
     todo_list = Todo.query.all()
     return render_template("index.html", todo_list=todo_list)
 
-@app.route("/add", methods=["POST"])
+@app.route("/add", methods=["POST","GET"])
 def add():
-    task = request.form.get("task")
-    new_todo = Todo(task=task, complete=False)
-    db.session.add(new_todo)
-    db.session.commit()
-    return redirect(url_for("index"))
+    form = TodoForm()
+    if form.validate_on_submit():
+        new_todo = Todo(task=form.task.data)
+        db.session.add(new_todo)
+        db.session.commit()
+        return redirect(url_for("index"))
+    return render_template("add.html", form=form)
 
 @app.route("/update/<int:todo_id>")
 def update(todo_id):
