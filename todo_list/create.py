5250def53ec4478f3674c0daff8ce30289ef1de7
Shane Forde <shaneforde02@googlemail.com>
2021-05-13 12:56:15 +0100

    SF connected to GCP, managed to add a dummy task

--- todo_list/create.py
+++ todo_list/create.py
@@ -1,5 +1,8 @@
-from app import db
+from app import db, Todo
 
 db.drop_all()
 db.create_all()
 
+test_task = Todo(task='First task',complete= True) # Extra: this section populates the table with an example entry
+db.session.add(test_task)
+db.session.commit()
